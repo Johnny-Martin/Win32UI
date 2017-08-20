a74e946cb0bc3990f1f4dfabb9862a607a2d1138
Johnny Martin <970803555@qq.com>
2017-08-20 19:46:03 +0800

    MSDN  D2D1 Demo

--- Direct2D/Demo/Demo.cpp
+++ Direct2D/Demo/Demo.cpp
@@ -1,180 +1,373 @@
-// Demo.cpp : 定义应用程序的入口点。
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
 //
+// Copyright (c) Microsoft Corporation. All rights reserved
 
 #include "stdafx.h"
 #include "Demo.h"
 
-#define MAX_LOADSTRING 100
-
-// 全局变量: 
-HINSTANCE hInst;                                // 当前实例
-WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
-WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
-
-// 此代码模块中包含的函数的前向声明: 
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+// Provides the application entry point.
+int WINAPI WinMain(
+	HINSTANCE /* hInstance */,
+	HINSTANCE /* hPrevInstance */,
+	LPSTR /* lpCmdLine */,
+	int /* nCmdShow */
+)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	// Use HeapSetInformation to specify that the process should
+	// terminate if the heap manager detects an error in any heap used
+	// by the process.
+	// The return value is ignored, because we want to continue running in the
+	// unlikely event that HeapSetInformation fails.
+	HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
 
-    // TODO: 在此放置代码。
+	if (SUCCEEDED(CoInitialize(NULL)))
+	{
+		{
+			DemoApp app;
 
-    // 初始化全局字符串
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_DEMO, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+			if (SUCCEEDED(app.Initialize()))
+			{
+				app.RunMessageLoop();
+			}
+		}
+		CoUninitialize();
+	}
 
-    // 执行应用程序初始化: 
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	return 0;
+}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DEMO));
+// DemoApp constructor
+DemoApp::DemoApp() :
+	m_hwnd(NULL),
+	m_pDirect2dFactory(NULL),
+	m_pRenderTarget(NULL),
+	m_pLightSlateGrayBrush(NULL),
+	m_pCornflowerBlueBrush(NULL)
+{
+}
 
-    MSG msg;
+// DemoApp destructor
+// Releases the application's resources.
+DemoApp::~DemoApp()
+{
+	SafeRelease(&m_pDirect2dFactory);
+	SafeRelease(&m_pRenderTarget);
+	SafeRelease(&m_pLightSlateGrayBrush);
+	SafeRelease(&m_pCornflowerBlueBrush);
 
-    // 主消息循环: 
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+}
 
-    return (int) msg.wParam;
+// Creates the application window and device-independent
+// resources.
+HRESULT DemoApp::Initialize()
+{
+	HRESULT hr;
+
+	// Initialize device-indpendent resources, such
+	// as the Direct2D factory.
+	hr = CreateDeviceIndependentResources();
+
+	if (SUCCEEDED(hr))
+	{
+		// Register the window class.
+		WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = DemoApp::WndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = sizeof(LONG_PTR);
+		wcex.hInstance = HINST_THISCOMPONENT;
+		wcex.hbrBackground = NULL;
+		wcex.lpszMenuName = NULL;
+		wcex.hCursor = LoadCursor(NULL, IDI_APPLICATION);
+		wcex.lpszClassName = L"D2DDemoApp";
+
+		RegisterClassEx(&wcex);
+
+
+		// Because the CreateWindow function takes its size in pixels,
+		// obtain the system DPI and use it to scale the window size.
+		FLOAT dpiX, dpiY;
+
+		// The factory returns the current system DPI. This is also the value it will use
+		// to create its own windows.
+		m_pDirect2dFactory->GetDesktopDpi(&dpiX, &dpiY);
+
+
+		// Create the window.
+		m_hwnd = CreateWindow(
+			L"D2DDemoApp",
+			L"Direct2D Demo App",
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			static_cast<UINT>(ceil(640.f * dpiX / 96.f)),
+			static_cast<UINT>(ceil(480.f * dpiY / 96.f)),
+			NULL,
+			NULL,
+			HINST_THISCOMPONENT,
+			this
+		);
+		hr = m_hwnd ? S_OK : E_FAIL;
+		if (SUCCEEDED(hr))
+		{
+			ShowWindow(m_hwnd, SW_SHOWNORMAL);
+			UpdateWindow(m_hwnd);
+		}
+	}
+
+	return hr;
+}
+
+// Creates resources that are not bound to a particular device.
+// Their lifetime effectively extends for the duration of the
+// application.
+HRESULT DemoApp::CreateDeviceIndependentResources()
+{
+	HRESULT hr = S_OK;
+
+	// Create a Direct2D factory.
+	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
+
+	return hr;
 }
 
 
-
-//
-//  函数: MyRegisterClass()
-//
-//  目的: 注册窗口类。
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
+// Creates resources that are bound to a particular
+// Direct3D device. These resources need to be recreated
+// if the Direct3D device dissapears, such as when the display
+// changes, the window is remoted, etc.
+HRESULT DemoApp::CreateDeviceResources()
 {
-    WNDCLASSEXW wcex;
+	HRESULT hr = S_OK;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	if (!m_pRenderTarget)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DEMO));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DEMO);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+		D2D1_SIZE_U size = D2D1::SizeU(
+			rc.right - rc.left,
+			rc.bottom - rc.top
+		);
 
-    return RegisterClassExW(&wcex);
+		// Create a Direct2D render target.
+		hr = m_pDirect2dFactory->CreateHwndRenderTarget(
+			D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(m_hwnd, size),
+			&m_pRenderTarget
+		);
+
+
+		if (SUCCEEDED(hr))
+		{
+			// Create a gray brush.
+			hr = m_pRenderTarget->CreateSolidColorBrush(
+				D2D1::ColorF(D2D1::ColorF::LightSlateGray),
+				&m_pLightSlateGrayBrush
+			);
+		}
+		if (SUCCEEDED(hr))
+		{
+			// Create a blue brush.
+			hr = m_pRenderTarget->CreateSolidColorBrush(
+				D2D1::ColorF(D2D1::ColorF::CornflowerBlue),
+				&m_pCornflowerBlueBrush
+			);
+		}
+	}
+
+	return hr;
 }
 
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目的: 保存实例句柄并创建主窗口
-//
-//   注释: 
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+// Discards device-dependent resources. These resources must be
+// recreated when the Direct3D device is lost.
+void DemoApp::DiscardDeviceResources()
 {
-   hInst = hInstance; // 将实例句柄存储在全局变量中
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+	SafeRelease(&m_pRenderTarget);
+	SafeRelease(&m_pLightSlateGrayBrush);
+	SafeRelease(&m_pCornflowerBlueBrush);
 }
 
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的:    处理主窗口的消息。
-//
-//  WM_COMMAND  - 处理应用程序菜单
-//  WM_PAINT    - 绘制主窗口
-//  WM_DESTROY  - 发送退出消息并返回
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+// Runs the main window message loop.
+void DemoApp::RunMessageLoop()
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 分析菜单选择: 
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 在此处添加使用 hdc 的任何绘图代码...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 }
 
-// “关于”框的消息处理程序。
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+// This method discards device-specific
+// resources if the Direct3D device dissapears during execution and
+// recreates the resources the next time it's invoked.
+HRESULT DemoApp::OnRender()
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	HRESULT hr = S_OK;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	hr = CreateDeviceResources();
+
+	if (SUCCEEDED(hr))
+	{
+		m_pRenderTarget->BeginDraw();
+
+		m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+
+		m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+
+		D2D1_SIZE_F rtSize = m_pRenderTarget->GetSize();
+
+		// Draw a grid background.
+		int width = static_cast<int>(rtSize.width);
+		int height = static_cast<int>(rtSize.height);
+
+		for (int x = 0; x < width; x += 10)
+		{
+			m_pRenderTarget->DrawLine(
+				D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
+				D2D1::Point2F(static_cast<FLOAT>(x), rtSize.height),
+				m_pLightSlateGrayBrush,
+				0.5f
+			);
+		}
+
+		for (int y = 0; y < height; y += 10)
+		{
+			m_pRenderTarget->DrawLine(
+				D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
+				D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y)),
+				m_pLightSlateGrayBrush,
+				0.5f
+			);
+		}
+
+		// Draw two rectangles.
+		D2D1_RECT_F rectangle1 = D2D1::RectF(
+			rtSize.width / 2 - 50.0f,
+			rtSize.height / 2 - 50.0f,
+			rtSize.width / 2 + 50.0f,
+			rtSize.height / 2 + 50.0f
+		);
+
+		D2D1_RECT_F rectangle2 = D2D1::RectF(
+			rtSize.width / 2 - 100.0f,
+			rtSize.height / 2 - 100.0f,
+			rtSize.width / 2 + 100.0f,
+			rtSize.height / 2 + 100.0f
+		);
+
+
+		// Draw a filled rectangle.
+		m_pRenderTarget->FillRectangle(&rectangle1, m_pLightSlateGrayBrush);
+
+		// Draw the outline of a rectangle.
+		m_pRenderTarget->DrawRectangle(&rectangle2, m_pCornflowerBlueBrush);
+
+		hr = m_pRenderTarget->EndDraw();
+	}
+
+	if (hr == D2DERR_RECREATE_TARGET)
+	{
+		hr = S_OK;
+		DiscardDeviceResources();
+	}
+
+	return hr;
+}
+
+
+//  If the application receives a WM_SIZE message, this method
+//  resizes the render target appropriately.
+void DemoApp::OnResize(UINT width, UINT height)
+{
+	if (m_pRenderTarget)
+	{
+		// Note: This method can fail, but it's okay to ignore the
+		// error here, because the error will be returned again
+		// the next time EndDraw is called.
+		m_pRenderTarget->Resize(D2D1::SizeU(width, height));
+	}
+}
+
+// Handles window messages.
+LRESULT CALLBACK DemoApp::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT result = 0;
+
+	if (message == WM_CREATE)
+	{
+		LPCREATESTRUCT pcs = (LPCREATESTRUCT)lParam;
+		DemoApp *pDemoApp = (DemoApp *)pcs->lpCreateParams;
+
+		::SetWindowLongPtrW(
+			hwnd,
+			GWLP_USERDATA,
+			PtrToUlong(pDemoApp)
+		);
+
+		result = 1;
+	}
+	else
+	{
+		DemoApp *pDemoApp = reinterpret_cast<DemoApp *>(static_cast<LONG_PTR>(
+			::GetWindowLongPtrW(
+				hwnd,
+				GWLP_USERDATA
+			)));
+
+		bool wasHandled = false;
+
+		if (pDemoApp)
+		{
+			switch (message)
+			{
+			case WM_SIZE:
+			{
+				UINT width = LOWORD(lParam);
+				UINT height = HIWORD(lParam);
+				pDemoApp->OnResize(width, height);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_DISPLAYCHANGE:
+			{
+				InvalidateRect(hwnd, NULL, FALSE);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_PAINT:
+			{
+				pDemoApp->OnRender();
+				ValidateRect(hwnd, NULL);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_DESTROY:
+			{
+				PostQuitMessage(0);
+			}
+			result = 1;
+			wasHandled = true;
+			break;
+			}
+		}
+
+		if (!wasHandled)
+		{
+			result = DefWindowProc(hwnd, message, wParam, lParam);
+		}
+	}
+
+	return result;
 }
