9befe714ebb0fbe4550ab89360047317cbca4cab
Johnny Martin <970803555@qq.com>
2017-08-23 18:11:23 +0800

    半透明窗口

--- GDI/Win32GDI+/Win32GDI+.cpp
+++ GDI/Win32GDI+/Win32GDI+.cpp
@@ -3,11 +3,13 @@
 
 #include "stdafx.h"
 #include "Win32GDI+.h"
+#include "Dwmapi.h"//毛玻璃
 
 #include<gdiplus.h>
 #include<iostream>
 using namespace Gdiplus;
 #pragma comment(lib, "GdiPlus.lib")
+#pragma comment(lib, "Dwmapi.lib")
 #pragma comment(linker, "/subsystem:\"console\" /entry:\"wWinMainCRTStartup\" ")
 
 
@@ -17,6 +19,7 @@ using namespace Gdiplus;
 HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+HRESULT EnableBlurBehind(HWND hwnd);			// 毛玻璃
 
 // 此代码模块中包含的函数的前向声明: 
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -118,7 +121,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    }
 
    ShowWindow(hWnd, nCmdShow);
+   EnableBlurBehind(hWnd);
    UpdateWindow(hWnd);
+  
 
    return TRUE;
 }
@@ -222,4 +227,108 @@ void TestGdiplus(HWND hwnd, HDC hdc)
 	);  
 
 	graphic.FillRectangle(&linGrBrush, 100, 100, width, height);
+}
+
+BOOL TryWin10(HWND hWnd)
+{
+	typedef enum _WINDOWCOMPOSITIONATTRIB
+	{
+		WCA_UNDEFINED = 0,
+		WCA_NCRENDERING_ENABLED = 1,
+		WCA_NCRENDERING_POLICY = 2,
+		WCA_TRANSITIONS_FORCEDISABLED = 3,
+		WCA_ALLOW_NCPAINT = 4,
+		WCA_CAPTION_BUTTON_BOUNDS = 5,
+		WCA_NONCLIENT_RTL_LAYOUT = 6,
+		WCA_FORCE_ICONIC_REPRESENTATION = 7,
+		WCA_EXTENDED_FRAME_BOUNDS = 8,
+		WCA_HAS_ICONIC_BITMAP = 9,
+		WCA_THEME_ATTRIBUTES = 10,
+		WCA_NCRENDERING_EXILED = 11,
+		WCA_NCADORNMENTINFO = 12,
+		WCA_EXCLUDED_FROM_LIVEPREVIEW = 13,
+		WCA_VIDEO_OVERLAY_ACTIVE = 14,
+		WCA_FORCE_ACTIVEWINDOW_APPEARANCE = 15,
+		WCA_DISALLOW_PEEK = 16,
+		WCA_CLOAK = 17,
+		WCA_CLOAKED = 18,
+		WCA_ACCENT_POLICY = 19, /////19！！
+		WCA_FREEZE_REPRESENTATION = 20,
+		WCA_EVER_UNCLOAKED = 21,
+		WCA_VISUAL_OWNER = 22,
+		WCA_LAST = 23
+	} WINDOWCOMPOSITIONATTRIB;
+
+	typedef struct _WINDOWCOMPOSITIONATTRIBDATA
+	{
+		WINDOWCOMPOSITIONATTRIB Attrib;
+		PVOID pvData;
+		SIZE_T cbData;
+	} WINDOWCOMPOSITIONATTRIBDATA;
+
+	typedef enum _ACCENT_STATE
+	{
+		ACCENT_DISABLED = 0,
+		ACCENT_ENABLE_GRADIENT = 1,
+		ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+		ACCENT_ENABLE_BLURBEHIND = 3,
+		ACCENT_INVALID_STATE = 4
+	} ACCENT_STATE;
+
+	typedef struct _ACCENT_POLICY
+	{
+		ACCENT_STATE AccentState;
+		DWORD AccentFlags;
+		DWORD GradientColor;
+		DWORD AnimationId;
+	} ACCENT_POLICY;
+
+	typedef BOOL(WINAPI*pfnSetWindowCompositionAttribute)(HWND, WINDOWCOMPOSITIONATTRIBDATA*);
+
+	HMODULE hUser = GetModuleHandle(L"user32.dll");
+	if (hUser)
+	{
+		pfnSetWindowCompositionAttribute setWindowCompositionAttribute = (pfnSetWindowCompositionAttribute)GetProcAddress(hUser, "SetWindowCompositionAttribute");
+		if (setWindowCompositionAttribute)
+		{
+			ACCENT_POLICY accent = { ACCENT_ENABLE_BLURBEHIND, 0, 0, 0 };
+			WINDOWCOMPOSITIONATTRIBDATA data;
+			data.Attrib = WCA_ACCENT_POLICY;
+			data.pvData = &accent;
+			data.cbData = sizeof(accent);
+			return setWindowCompositionAttribute(hWnd, &data);
+		}
+	}
+}
+
+HRESULT EnableBlurBehind(HWND hwnd)
+{
+	//开启半透明
+	DWORD exStyle = ::GetWindowLong(hwnd, GWL_EXSTYLE);
+	exStyle |= WS_EX_LAYERED;
+	::SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
+	::SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 100, LWA_ALPHA);//LWA_COLORKEY
+	//::UpdateLayeredWindow(hwnd, );
+
+	//启动毛玻璃
+	if (TryWin10(hwnd))
+		return S_OK;
+
+	HRESULT hr = S_OK;
+
+	// Create and populate the blur-behind structure.
+	DWM_BLURBEHIND bb = { 0 };
+
+	// Specify blur-behind and blur region.
+	bb.dwFlags = DWM_BB_ENABLE;
+	bb.fEnable = true;
+	bb.hRgnBlur = NULL;
+
+	// Enable blur-behind.
+	hr = DwmEnableBlurBehindWindow(hwnd, &bb);
+	if (SUCCEEDED(hr))
+	{
+		// ...
+	}
+	return hr;
 }
